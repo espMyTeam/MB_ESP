5d385aeb231da1fa0063ce7c17ea2495175c3e51
bngesp <bassiroungom26@gmail.com>
2016-05-26 21:50:08 +0000

    fonctions de la banque

---
+++ services/web/Constantes.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 5/26/16
+ * Time: 9:16 PM
+ */
+class Constantes
+{
+    public static $NUM_COMPTE_INVALIDE=-1;
+    public static $SOLDE_INSUFFISANT=-2;
+    public static $SUCCES=0;
+    public static $ERREUR_REQUETTE=-3;
+    public static $ERREUR_TRANSFERT=-4;
+    public static $COMPTE_DESTINATAIRE_INVALIDE=-2;
+    public static $COMPTE_SOURCE_INVALIDE=-1;
+    
+}
--- services/web/classes.php
+++ services/web/classes.php
@@ -1,17 +1,79 @@
 <?php
 class Compte {
 	private $base;
+	
 	function __construct($base){
 		$this->base=$base;
 	}
 
+	//cete fonction permet a un abonne de consulter la banque
 	function  consultation($numcpt){
 		$check=$this->base->prepare("select solde from compte WHERE idCompte=:n1");
 		$check->execute(array(
 			':n1' => $numcpt
 		));
+		$value=$check->fetch();
+		return $value[0]!=null ? $value[0]: Constantes::$NUM_COMPTE_INVALIDE;
+	}
 
-		return $check;//->fetch();
+	function debiter($montant, $numcpt)
+	{
+		$solde=$this->consultation($numcpt);
+		if($solde!=Constantes::$NUM_COMPTE_INVALIDE)
+		{
+			if($solde>$montant)
+			{
+				$newSolde=$solde-$montant;
+				$req=$this->base->prepare("update compte set solde=:n2  WHERE idCompte=:n1");
+				$req->execute(array(
+					':n1' => $numcpt,
+					':n2' => $newSolde
+				));
+				return Constantes::$SUCCES;
+			}else return Constantes::$NUM_COMPTE_INVALIDE;
+
+		}else
+			return Constantes::$SOLDE_INSUFFISANT;
+	}
+
+	function crediter($montant, $numcpt){
+		$solde=$this->consultation($numcpt);
+		if($solde!=Constantes::$NUM_COMPTE_INVALIDE){
+			$newSolde=$solde+$montant;
+			$req=$this->base->prepare("update compte set solde=:n2  WHERE idCompte=:n1");
+			$req->execute(array(
+				':n1' => $numcpt,
+				':n2' => $newSolde
+			));
+			return Constantes::$SUCCES;
+		}else{
+			return Constantes::$NUM_COMPTE_INVALIDE;
+		}
+
+	}
+
+	function transfert($numcpt, $numcptdest, $montant){
+		$solde=$this->consultation($numcpt);
+		if($solde!=Constantes::$NUM_COMPTE_INVALIDE)
+		{
+			$verification=$this->debiter($montant,$numcpt);
+			
+			if($verification==Constantes::$SUCCES)
+			{
+				$var=$this->crediter($montant, $numcptdest);
+				if($var!=Constantes::$SUCCES) 
+				{
+					$this->crediter($montant, $numcpt);
+					return Constantes::$ERREUR_TRANSFERT;
+				}
+				return Constantes::$SUCCES;
+			}
+			elseif ($verification==Constantes::$NUM_COMPTE_INVALIDE) return Constantes::$COMPTE_SOURCE_INVALIDE; 
+			
+			else return Constantes::$SOLDE_INSUFFISANT;
+					
+		}else return Constantes::$ERREUR_TRANSFERT;
+			
 	}
 }
 
